ddaa2562d7bf6bc3a840db415fec6a54555bc49c
venn <vennboddula@gmail.com>
2021-09-20 21:18:28 +0530

    added navbar functions

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ $(document).ready(function(){
         }
     });
 });
+$('.navbar .menu li a').click(function(){
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+});
+
+// toggle menu/navbar script
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
 function download()
 {
     var link = document.createElement("a");
